9d5e546dd6a546446f42480c4734122d50c215c7
zamnh <hnamnguyen.hy@gmail.com>
2024-06-01 21:41:58 +0700

    add: #2 - flavor, firebase configuration

--- lib/main_free.dart
+++ lib/main_free.dart
@@ -1,4 +1,4 @@
-import 'package:brick_breaker/firebase_options.dart';
+import 'package:brick_breaker/firebase_options_free.dart';
 import 'package:brick_breaker/src/widgets/game_app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
--- lib/src/config.dart
+++ lib/src/config.dart
@@ -15,7 +15,7 @@ const brickColors = [
 ];
 
 const gameWidth = 820.0;
-const gameHeight = 1600.0;
+const gameHeight = 1500.0;
 
 const ballRadius = gameWidth * 0.02;
 
