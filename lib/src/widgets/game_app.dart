a559a22079f408a28781e724e705cb3143be99c6
quyetnv <quyetnv.mlhn@gmail.com>
2024-06-02 20:42:54 +0700

    update: game state

--- lib/src/widgets/game_app.dart
+++ lib/src/widgets/game_app.dart
@@ -58,6 +58,20 @@ class _GameAppState extends State<GameApp> {
                         height: gameHeight,
                         child: GameWidget.controlled(
                           gameFactory: () => game,
+                          // overlayBuilderMap: {
+                          // PlayState.welcome.name: (context, game) => const OverlayScreen(
+                          //   title: 'TAP TO PLAY',
+                          //   subtitle: 'Use arrow keys or swipe',
+                          // ),
+                          // PlayState.gameOver.name: (context, game) => const OverlayScreen(
+                          //   title: 'G A M E   O V E R',
+                          //   subtitle: 'Tap to Play Again',
+                          // ),
+                          // PlayState.won.name: (context, game) => const OverlayScreen(
+                          //   title: 'Y O U   W O N ! ! !',
+                          //   subtitle: 'Tap to Play Again',
+                          // ),
+                          // },
                         ),
                       ),
                     ),
